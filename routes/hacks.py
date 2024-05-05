5d4bd037eb2bdf1e562910a4fbbc61e1eac3adec
GianfrancoD <gianfranco.dfiore@gmail.com>
2024-05-04 22:21:22 -0400

    Hack Python/Back

--- routes/hacks.py
+++ routes/hacks.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, request
-
 class Alluser:
     def __init__(self, blueprint):
         self.blueprint = blueprint
@@ -9,28 +8,105 @@ class Alluser:
         self.blueprint.route('/user', methods=['POST'])(self.poster)
         self.blueprint.route('/user', methods=['DELETE'])(self.delete)
         self.blueprint.route('/user', methods=['PUT'])(self.puts)
+        self.blueprint.route('/api/v1/users', methods=['GET'])(self.getlist)
+        self.blueprint.route('/api/v1/user', methods=['POST'])(self.postlist)
+        self.blueprint.route('/api/v1/user/add', methods=['POST'])(self.postagain)
+        self.blueprint.route('/api/v1/user/create', methods=['POST'])(self.create_user)
         return self.blueprint
 
+    """HACK 1"""
     def getter(self):
         if request.method == 'GET':
             return jsonify({'payload': 'success'}), 200
         else:
             return jsonify({'error': 'It is Invalid Request'}), 405
 
+    """HACK 2"""
     def poster(self):
         if request.method == 'POST':
             return jsonify({'payload': 'success'}), 200
         else:
             return jsonify({'error': 'It is Invalid Request'}), 405
     
+    """HACK 3"""
     def delete(self):
         if request.method == 'DELETE':
             return jsonify({'payload': 'success'}), 200
         else:
             return jsonify({'error': 'It is Invalid Request'}), 405
         
+    """HACK 4"""
     def puts(self):
         if request.method == 'PUT':
             return jsonify({'payload': 'success', 'error': False}), 200
         else:
             return jsonify({'error': 'It is Invalid Request'}), 405
+        
+    """HACK 5"""
+    def getlist(self):
+        if request.method == 'GET':
+            return jsonify({'payload': []}), 200
+        else:
+            return jsonify({'error': 'It is Invalid Request'}), 405
+        
+    """HACK 6"""
+    """POR RESOLVER"""
+    def postlist(self):
+        if request.method == 'POST':
+            try:
+                email = request.args.get('email')
+                name = request.args.get('name')
+                if email and name:
+                    payload = {'email': email, 'name': name}
+                    return jsonify({'payload': payload}), 201
+                else:
+                    error_response = {'error': 'Invalid request'}
+                    return jsonify(error_response), 400
+            except Exception as e:
+                error_response = {'error': str(e)}
+                return jsonify(error_response), 500
+        
+
+    """HACK 7"""
+    def postagain(self):
+        if request.method == 'POST':
+            try:
+                email = request.form.get('email')
+                name = request.form.get('name')
+                id = request.form.get('id')
+
+                if email and name and id:
+                    return jsonify({
+                            'payload' : {
+                                'email': email,
+                                'name': name,
+                                'id': id
+                                }
+                        }), 200
+                else:
+                    return jsonify({
+                        'error': 'error'
+                        }),405
+            except Exception as e:
+                return jsonify({
+                        'error': str(e)
+                    }), 500
+
+    """HACK 8"""
+    def create_user(self):
+        try:
+            data = request.get_json()
+            email = data["email"]
+            name = data["name"]
+            id = data["id"]
+            return jsonify({
+                        'payload': {
+                            'email':email, 
+                            'name':name, 
+                            'id':id 
+                                }}), 200
+        except Exception as e:
+            error_response = {
+               'error': str(e)
+         }
+            return jsonify(error_response), 400
